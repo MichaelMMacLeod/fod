919630928c74a1b145c1979ca2ea8a52daeb5bf3
Michael MacLeod <MichaelMMacLeod@gmail.com>
2017-06-01 20:52:58 -0700

    Create client gui when connected to server

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,17 +1,52 @@
 package client;
 
+import client.gui.GamePanel;
 import server.FODServer;
 
+import javax.swing.*;
+import java.awt.*;
 import java.io.IOException;
 import java.net.Socket;
 
 public class Client {
+    private static GamePanel gamePanel;
+
     public static void main(String[] args) throws IOException {
         Client client = new Client();
         client.connect(args[0]);
+
+        System.setProperty("sun.java2d.opengl", "true");
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int width = (int) screenSize.getWidth();
+        int height = (int) screenSize.getHeight();
+
+        gamePanel = new GamePanel(width, height);
+
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                startGUI();
+            }
+        });
     }
 
     private void connect(String ip) throws IOException {
         Socket socket = new Socket(ip, FODServer.PORT);
+    }
+
+    private static void startGUI() {
+        JFrame frame = new JFrame("Sonder");
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setUndecorated(false);
+
+        gamePanel.setBackground(Color.WHITE);
+
+        frame.add(gamePanel);
+        frame.pack();
+
+        frame.setLocationRelativeTo(null);
+
+        frame.setVisible(true);
     }
 }
---
+++ src/client/gui/GamePanel.java
@@ -0,0 +1,23 @@
+package client.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel {
+    private final int width, height;
+
+    public GamePanel(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(width, height);
+    }
+}
